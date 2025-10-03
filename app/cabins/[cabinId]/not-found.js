d9b6d531f238a8945a9f6e772ffc8e3ccc0817d0
Maciej <93821776+nihilno@users.noreply.github.com>
2025-10-03 19:34:57 +0200

    Update not-found.js

--- app/cabins/[cabinId]/not-found.js
+++ app/cabins/[cabinId]/not-found.js
@@ -7,7 +7,7 @@ export const metadata = {
 function NotFound() {
   return (
     <main className="mt-4 space-y-6 text-center">
-      <h1 className="text-3xl font-semibold">This cabin could not be found</h1>
+      <h1 className="text-3xl font-semibold">This could not be found</h1>
       <Link
         href="/cabins"
         className="bg-accent-500 text-primary-800 inline-block px-6 py-3 text-lg"
